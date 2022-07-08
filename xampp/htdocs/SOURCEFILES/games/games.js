860204e63422ad6b72b14ff44d61aae780c14c2d
merger3 <merger3@outlook.com>
2022-07-08 02:07:20 -0400

    css changes

--- xampp/htdocs/SOURCEFILES/games/games.js
+++ xampp/htdocs/SOURCEFILES/games/games.js
@@ -1,19 +1,24 @@
+const DIV = "<div></div>"
 var xhttp = new XMLHttpRequest(); xhttp.onreadystatechange = 
 function() { 
 if (this.readyState == 4 && this.status == 200) { 
     var gamesList = JSON.parse(this.responseText)
+    //gamesList.length
     for (var i = 0; i < gamesList.length; i++) {
-        var newGame = $('<div></div>').addClass("game");
-        $(newGame).append($("<img src='" + gamesList[i].logo + ".png'/>").addClass("logo"))
-        $(newGame).append($("<img src='" + gamesList[i].logo + "_gameplay.png'/>").addClass("gameplay"))
-        $(newGame).append($('<div></div>').text(gamesList[i].description))
+        var newGame = $(DIV).addClass("game");
+        $(newGame).append($("<img src='" + gamesList[i].logo + ".png'/>").addClass("poster"))
+        var textFields = $(DIV).addClass("textFields")
+        $(textFields).append($(DIV).text(gamesList[i].name))
+        $(textFields).append($(DIV).text(gamesList[i].description))
+        $(newGame).append(textFields)
         $(newGame).click(function() {
             $(".game").css("border", "3px dotted black");
             $(this).css("border", "3px solid red");
         });
-        $(".demo").append(newGame)
+        $(".menu").append(newGame)
     }
 } 
 }; 
 xhttp.open("GET", "games.json", true); 
 xhttp.send(); 
+// $(newGame).append($("<img src='" + gamesList[i].logo + "_gameplay.png'/>").addClass("gameplay"))
